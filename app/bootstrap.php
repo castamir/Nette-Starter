f012658edc35917c776a539d98f198af91b236df
castamir <miras.paulik@email.cz>
2016-04-23 10:25:48 +0200

    initial commit

---
+++ app/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configurator = new Nette\Configurator;
+
+//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+@mkdir(__DIR__ . '/../temp/log');
+$configurator->enableDebugger(__DIR__ . '/../temp/log');
+
+$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+$configurator->createRobotLoader()
+    ->addDirectory(__DIR__)
+    ->register();
+
+$configurator->addConfig(__DIR__ . '/config/config.neon');
+
+$container = $configurator->createContainer();
+
+return $container;
---
+++ app/services/RouterFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use Nette;
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
+
+class RouterFactory
+{
+
+    /**
+     * @return Nette\Application\IRouter
+     */
+    public static function createRouter()
+    {
+        $router = new RouteList;
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+        return $router;
+    }
+
+}
